5da37fc92043413b20e196bfc162e2b56a363598
Maksim <maksimovseo@gmail.com>
2026-05-23 21:58:42 +0300

    add Prisma, PostgreSQL, interfaces and api.ts

--- components/Rating/Rating.props.ts
+++ components/Rating/Rating.props.ts
@@ -3,5 +3,6 @@ import { ComponentProps } from 'react';
 export interface RatingProps extends ComponentProps<'div'> {
   isEditable?: boolean;
   rating: number;
+  // add ? to make setRating optional, so Rating can be used without editing mode
   setRating: (rating: number) => void;
 }
