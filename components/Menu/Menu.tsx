3a05d026870a1449a8419a5602207989af9ba788
Maksim <maksimovseo@gmail.com>
2026-06-08 21:15:15 +0300

    replace category menu state with route-based navigation, add menu styles

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,18 +1,31 @@
 'use client';
 
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import cn from 'classnames';
+import styles from './Menu.module.css';
 import { useAppContext } from '@/context/app.context';
 
 export const Menu = () => {
-  const { categories, openedCategory, setOpenedCategory } = useAppContext();
+  const { categories } = useAppContext();
+  const pathname = usePathname();
 
   return (
-    <div>
-      <ul>
-        {categories.map((cat) => (
-          <li key={cat.id} onClick={() => setOpenedCategory(cat.alias)}>
-            {cat.name}
-          </li>
-        ))}
+    <div className={styles.menu}>
+      <ul className={styles.list}>
+        {categories.map((cat) => {
+          const isActive = pathname === `/categories/${cat.alias}`;
+          return (
+            <li
+              key={cat.id}
+              className={cn(styles.item, {
+                [styles.active]: isActive,
+              })}
+            >
+              <Link href={`/categories/${cat.alias}`}>{cat.name}</Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
